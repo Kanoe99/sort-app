111a93fe40954c28a7891efc5805a4e04d091c60
Kanoe99 <kanoe.s99@gmail.com>
2024-11-29 13:45:34 +0500

    README: added

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth')->group(function () {
     Route::get('/', [PrinterController::class, 'index']);
 
+
     //prevent manual accesing non get routes
 
     Route::get('/printers/create', [PrinterController::class, 'create']);
